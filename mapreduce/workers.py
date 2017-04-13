87d17922667e1e6de43f9356d1c6ad74d16daae0
Jiadong Zhu <jiadong.zhu@nyu.edu>
2017-04-13 10:47:09 -0400

    detached process fixed

--- mapreduce/workers.py
+++ mapreduce/workers.py
@@ -8,22 +8,31 @@ Using circus to spawn all the worker process
 '''
 
 import os
-import time
+import signal
 import sys
 import subprocess
 
 from mapreduce import worker, utils, manifest
 
+
+
+apps = []
+
+def sigterm_hdl(signo, stack):
+    map(lambda app: app.kill(), apps)
+    sys.exit(0)
+
 if __name__ == '__main__':
-    apps = []
     env = os.environ.copy()
+    signal.signal(signal.SIGTERM, sigterm_hdl)
+    signal.signal(signal.SIGTSTP, sigterm_hdl)
+    signal.signal(signal.SIGINT, sigterm_hdl)
     for i in range(0, manifest.WORKER_NUM):
         port = manifest.BASE_PORT + i
         proc = subprocess.Popen(args=["python", "-u", "-m", "mapreduce.worker", str(port)],
                                 stdout=sys.stdout,
                                 stderr=sys.stderr,
-                                env=env,
-                                encoding="utf-8")
+                                env=env)
         apps.append(proc)
 
     try:
